d29cdf8bbc34adb3eb038f77775bd0aac09b1a83
David Woolsey <woolsey2316@gmail.com>
2026-03-13 10:37:16 +1100

    fix: casing

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 # Create your models here.
-class PublishManager(models.manager):
+class PublishManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 

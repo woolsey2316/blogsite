1f163b2be7b941b63d83b0a539e7899e814df6c3
David Woolsey <woolsey2316@gmail.com>
2026-07-02 12:43:13 +1000

    fix: nix-shell combined with venv

--- shell.nix
+++ shell.nix
@@ -1,18 +1,28 @@
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
-  # Add the Python interpreter and native dependencies here
-  packages = [
-    (pkgs.python3.withPackages (python-pkgs: [
-      python-pkgs.numpy
-      python-pkgs.pandas
-      python-pkgs.requests
-    ]))
+  # Packages installed inside the environment
+  buildInputs = with pkgs; [
+    python3
+    python3Packages.pip
+    python3Packages.virtualenv
+    
+    # Native C dependencies frequently needed by python wheels
+    stdenv.cc.cc.lib
+    zlib
+    glibc
   ];
 
-  # Run commands automatically upon entering the shell
+  # Fixes issues with pre-compiled wheels looking for standard libraries
   shellHook = ''
-    echo "Python development environment loaded!"
-    python --version
+    export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [ pkgs.stdenv.cc.cc.lib pkgs.zlib ]}:$LD_LIBRARY_PATH"
+    
+    # Automate virtualenv creation and activation
+    if [ ! -d ".venv" ]; then
+      python -m venv .venv
+    fi
+    source .venv/bin/activate
+    
+    echo "Python environment active. Run 'pip install -r requirements.txt' to update packages."
   '';
 }
